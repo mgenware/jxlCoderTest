1c55fcda4aa722872be7ab3e13b7f48f7f90f0e4
Mgen <m@mgenware.com>
2023-10-12 15:46:07 +0800

    Initial Commit

---
+++ jxlCoderTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  jxlCoderTest
+//
+//  Created by YuanYuan Liu on 2023/10/12.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
